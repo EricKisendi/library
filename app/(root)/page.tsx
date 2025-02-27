b1beef04f17064b67f302428cab8e17a616cdb4d
EricKisendi <erickisendi18@gmail.com>
2025-02-27 15:49:04 +0300

    Startup creation of the header and adding of new fonts

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,10 @@
 import { Button } from "@/components/ui/button";
 
 const Home = () => (
-    <div className="font-bebas-neue bg-chart-3">
+    <div>
       <Button >
         What The F*ck
       </Button>
-      <div className="book.btn">
-        Book Button
-      </div>
     </div>
 )
 
