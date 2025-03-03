3ab2b7f47dcf40beddd649163e089d69724d9cb6
EricKisendi <erickisendi18@gmail.com>
2025-03-03 21:02:05 +0300

    Setup of the Authentication pages

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,18 @@
+import AuthForm from '@/components/AuthForm'
+import React from 'react'
+import { signInSchema } from '@/lib/validations'
+
+const page = () => {
+  return (
+    <AuthForm 
+        type='SIGN_IN'
+        schema={signInSchema}
+        defaultValues = {{
+        email: '',
+        password: ''
+        }}
+    />
+  )
+}
+
+export default page
